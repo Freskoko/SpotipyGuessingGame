1da0cbcfeb90c1b96aa08af5d071553401918050
HenrikLaptop <frishcoco@gmail.com>
2023-03-16 23:46:11 +0100

    webiste design

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { useState, useEffect } from 'react';
 
 //TODO SOME STUFF
 
-function DataComponent() {
-  const [data, setData] = useState([]);
+
+function CorrectCounter({correctamount}){
+
+  return(
+
+    <p>{ correctamount }</p>
+
+  )
+}
+
+
+function DataComponent({ setCorrectAmount }) {
+  const [song1, setSong1] = useState([]);
+  const [song2, setSong2] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  function handleButtonClick() {
+
+
+  function GrabData(){
     setIsLoading(true);
-    fetch('http://localhost:5000/data')
+    return fetch('http://localhost:5000/data')
       .then(res => res.json())
       .then(data => {
         console.log(data)
-        setData(data);
+        setIsLoading(false);
+        setSong1(data);
+        return data;
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }
+
+  function handleButtonClick() {
+    setIsLoading(true);
+    Promise.all([GrabData(), GrabData()])
+      .then(([data1, data2]) => {
+        setSong1(data1);
+        setSong2(data2);
         setIsLoading(false);
       })
       .catch(err => {
@@ -25,30 +54,89 @@ function DataComponent() {
     handleButtonClick();
   }, []);
 
+
+  function handleImageClickSong1() {
+    // console.log("clicked1")
+    // console.log(song1["popularity"])
+    if ( song1["popularity"] >= song2["popularity"] ) {
+      console.log("Correct!");
+
+      setCorrectAmount(prevCount => prevCount + 1);
+    }
+  }
+  function handleImageClickSong2() {
+    console.log("clicked2 ")
+    if (song2["popularity"] >= song1["popularity"]) {
+      console.log("Correct!");
+
+      setCorrectAmount(prevCount => prevCount + 1);
+
+    }
+  }
+
   return (
-    <div>
+    <div className="centeritems"> 
       <button onClick={handleButtonClick} disabled={isLoading}>
         {isLoading ? 'Loading...' : 'Get new data'}
-      </button>
+      </button>¨
 
-          <div>
-      {Object.keys(data).map((key) => (
-        <div key={key}>
-          <span>{key}: </span>
-          <span>{data[key]}</span>
-        </div>
-      ))}
-    </div>
+
+      <div class="sideways">
+
+          <div class="ontop">
+            
+              <div>
+
+              {song1["artist"]}
+              {song1["name"]}
+
+              </div>
+
+              
+              <button onClick={handleImageClickSong1}>
+                <img src={song1["image"]} width="212" height="700"/>
+              </button>
+              
+
+          </div>
+
+
+
+          <div class="ontop">
+            
+              <div>
+
+              {song2["artist"]}
+              {song2["name"]}
+
+              </div>
+
+          
+              <button onClick={handleImageClickSong2}>
+                <img src={song2["image"]} width="212" height="700"/>
+              </button>
+
+          </div>
+
+
+      </div>
 
     </div>
   );
 }
 
 function App() {
+  const [correctamount,setCorrectAmount] = useState(0)
+
   return (
-    <div>
-      <DataComponent />
-      <h1>hello</h1>
+    <div className="centeritems">
+
+      <h1>Guess the song!</h1>  
+
+      <CorrectCounter correctamount={correctamount}/>
+
+      <DataComponent setCorrectAmount={setCorrectAmount}/>
+      
     </div>
   );
 }
