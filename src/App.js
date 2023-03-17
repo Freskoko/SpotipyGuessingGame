9d7426108a2eadc45d5535d4203dbfa804cff61c
HenrikLaptop <frishcoco@gmail.com>
2023-03-17 13:05:53 +0100

    website changes - button, checkmark/x

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
-//TODO SOME STUFF
+function CorrectSymbol({visibility, image}) {
+
+  return (
+    <div>
+      {/* {visibility && <img src="checkmark.png" width="50" height="50"/>} */}
+
+      {visibility &&    <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            zIndex: 9999,
+          }}
+        >
+          <img src={image} alt="overlay image" />
+        </div>}
+
+    </div>
+
+  ) 
+}
 
 
 function CorrectCounter({correctamount}){
 
   return(
 
-    <p>{ correctamount }</p>
+    <h2>Score: { correctamount }</h2>
 
   )
 }
 
 
-function DataComponent({ setCorrectAmount }) {
+function DataComponent({ setCorrectAmount, setVisibility , setImage}) {
   const [song1, setSong1] = useState([]);
   const [song2, setSong2] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-
 
   function GrabData(){
     setIsLoading(true);
@@ -62,8 +85,34 @@ function DataComponent({ setCorrectAmount }) {
       console.log("Correct!");
 
       setCorrectAmount(prevCount => prevCount + 1);
+
+      setImage("checkmark.png")
+
+      setVisibility(true);
+
+      // Set the visibility back to true after 3 seconds
+      setTimeout(() => {
+        setVisibility(false);
+      }, 2000);
+
     }
+
+    else{
+      setCorrectAmount(0)
+
+      setImage("redx.png")
+
+      setVisibility(true);
+
+      // Set the visibility back to true after 3 seconds
+      setTimeout(() => {
+        setVisibility(false);
+      }, 2000);
+
+    }
+    handleButtonClick()
   }
+
   function handleImageClickSong2() {
     console.log("clicked2 ")
     if (song2["popularity"] >= song1["popularity"]) {
@@ -71,7 +120,31 @@ function DataComponent({ setCorrectAmount }) {
 
       setCorrectAmount(prevCount => prevCount + 1);
 
+      setImage("checkmark.png")
+
+      setVisibility(true);
+
+      // Set the visibility back to true after 3 seconds
+      setTimeout(() => {
+        setVisibility(false);
+      }, 2000);
+
     }
+
+    else{
+      setCorrectAmount(0)
+
+      setImage("redx.png")
+
+      setVisibility(true);
+
+      // Set the visibility back to true after 3 seconds
+      setTimeout(() => {
+        setVisibility(false);
+      }, 2000);
+
+    }
+    handleButtonClick()
   }
 
 return (
@@ -84,14 +157,14 @@ return (
         <div className="ontop">
           <div>{song1.artist}</div>
           <div>{song1.name}</div>
-          <button onClick={handleImageClickSong1}>
+          <button onClick={handleImageClickSong1} class ="inpbuttonimg">
             <img src={song1.image} width="512" height="700" />
           </button>
         </div>
         <div className="ontop">
           <div>{song2.artist}</div>
           <div>{song2.name}</div>
-          <button onClick={handleImageClickSong2}>
+          <button onClick={handleImageClickSong2} class ="inpbuttonimg">
             <img src={song2.image} width="512" height="700" />
           </button>
         </div>
@@ -103,15 +176,25 @@ return (
 
 function App() {
   const [correctamount,setCorrectAmount] = useState(0)
+  const [isVisible,setVisibility] = useState(false);
+  const [image, setImage] = useState("checkmark.png")
 
   return (
     <div className="centeritems">
 
-      <h1>Guess the song!</h1>  
+      <h1>Which song is more popular?</h1>  
+
+      <CorrectSymbol visibility={isVisible} image={image}/>
 
       <CorrectCounter correctamount={correctamount}/>
 
-      <DataComponent setCorrectAmount={setCorrectAmount}/>
+      <DataComponent 
+        setCorrectAmount={setCorrectAmount} 
+        setVisibility={setVisibility} 
+        setImage ={setImage}
+        />
+
+     
       
     </div>
   );
